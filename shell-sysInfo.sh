08c88da03c8943ca77d4fb15c4f23a1ac8f2a118
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2019-03-01 16:00:34 +0530

    Add files via upload
    additional shell programming to display various system details and hardware information

---
+++ shell-sysInfo.sh
@@ -0,0 +1,26 @@
+clear 
+echo
+echo
+uname -s
+uname -n
+uname -r
+uname -v
+uname -m
+uname -p
+uname -i
+uname -o
+echo
+echo
+cat /etc/shells
+echo
+echo
+echo 
+lscpu 
+echo
+echo
+echo
+free -m
+echo
+echo
+echo
+dmidecode -t memory 
